38eb9c55f72f386a1f700ad0e96e342d449c3c40
Sameer-Ansari-0602 <ansari.sameer1212005@gmail.com>
2026-07-06 19:34:04 +0530

    Add Vercel backend entrypoint

---
+++ backend/api/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const mainRouter = require("../routes/main.router");
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.json());
+
+const allowedOrigins = [
+  "https://code-hub-frontend-ochre.vercel.app",
+  "https://code-hub-live.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "http://127.0.0.1:5173",
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  } else if (!origin) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS",
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.error("Error in mongodb connection", err));
+
+app.use("/", mainRouter);
+
+module.exports = app;
